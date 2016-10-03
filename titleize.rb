efaa8c425cf4df46d0557b0afb6dc06e4a9ae7d9
Nina Louw <ninalouw@mac.com>
2016-10-03 11:48:44 -0700

    Add titleize.rb to Quiz solutions

---
+++ titleize.rb
@@ -0,0 +1,32 @@
+# 7. Implement the following code in Ruby: Create a module called HelperMethods
+# that include a method called `titleize` that does the following: it takes in a
+# string and returns the string back after capitalizing each word in that string.
+# For example if you pass to the method a string "hello world" you should get back
+#  "Hello World". The methods should not capitalize the following words: in, the,
+#  of, and, or, from.
+
+ module HelperMethods
+
+def titleize(str)
+
+  final_arr = []
+  no_caps = ['In', 'The','Of', 'And', 'Or', 'From']
+myArray = str.split
+
+myArray.each_with_index do |word,index|
+    word.capitalize!
+  if index > 0
+       no_caps.each do |bad_word|
+          if word == bad_word
+              word.downcase!
+              break
+            end
+          end
+        end
+      end
+final_arr << myArray.join(" ")
+end
+module_function :titleize
+end
+string = "the lord of the rings and the philosopher in the dark"
+puts HelperMethods.titleize(string)
